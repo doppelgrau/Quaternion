7003cf5f37d501940c4a2ec46ff0c9541abc2d18
Johannes Formann <johannes.formann@uni-duisburg-essen.de>
2020-03-24 01:38:00 +0100

    removed copysign

--- Quaternion.cpp
+++ Quaternion.cpp
@@ -52,8 +52,11 @@ void Quaternion::to_euler_rotation(double *roll, double *pitch, double *yaw) {
 
  // pitch (y-axis rotation)
  double sinp = 2 * (a * c - d * b);
- if (std::abs(sinp) >= 1)
-     *pitch = std::copysign(M_PI / 2, sinp); // use 90 degrees if out of range
+ if (std::abs(sinp) >= 1)	// use +/-90 degrees if out of range
+	if ( simp > 0)
+		*pitch = M_PI / 2; 
+	else
+		*pitch = -1 * (M_PI / 2); 
  else
      *pitch = std::asin(sinp);
 
